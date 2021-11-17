bd6ce6a8252929f6d919519a0717b413d7642edb
JOHN CARLOS ARRIETA ARRIETA <johncarlosarrietaarrieta@gmail.com>
2021-11-17 08:05:37 -0500

    Actualizado desde Netbeans 12.2 en la clase #17 del Grupo 16 MisionTic de La Sergio

---
+++ src/main/java/co/gov/mintic/misiontic/usa/ciclo3/modelo/Estudiante.java
@@ -0,0 +1,110 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package co.gov.mintic.misiontic.usa.ciclo3.modelo;
+
+/**
+ *
+ * @author JOHN CARLOS ARRIETA ARRIETA
+ */
+public class Estudiante {
+      private String nombre;
+      private float frontend_reto2;
+      private float frontend_reto3;
+      private float frontend_reto4;
+      private float frontend_reto5;
+      private String mensaje_r2;
+      private String mensaje_r3;
+      private String mensaje_r4;
+      private String mensaje_r5;
+      private float promedio;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public float getFrontend_reto2() {
+        return frontend_reto2;
+    }
+
+    public void setFrontend_reto2(float frontend_reto2) {
+        this.frontend_reto2 = frontend_reto2;
+    }
+
+    public float getFrontend_reto3() {
+        return frontend_reto3;
+    }
+
+    public void setFrontend_reto3(float frontend_reto3) {
+        this.frontend_reto3 = frontend_reto3;
+    }
+
+    public float getFrontend_reto4() {
+        return frontend_reto4;
+    }
+
+    public void setFrontend_reto4(float frontend_reto4) {
+        this.frontend_reto4 = frontend_reto4;
+    }
+
+    public float getFrontend_reto5() {
+        return frontend_reto5;
+    }
+
+    public void setFrontend_reto5(float frontend_reto5) {
+        this.frontend_reto5 = frontend_reto5;
+    }
+
+    public String getMensaje_r2() {
+        return mensaje_r2;
+    }
+
+    public void setMensaje_r2(String mensaje_r2) {
+        this.mensaje_r2 = mensaje_r2;
+    }
+
+    public String getMensaje_r3() {
+        return mensaje_r3;
+    }
+
+    public void setMensaje_r3(String mensaje_r3) {
+        this.mensaje_r3 = mensaje_r3;
+    }
+
+    public String getMensaje_r4() {
+        return mensaje_r4;
+    }
+
+    public void setMensaje_r4(String mensaje_r4) {
+        this.mensaje_r4 = mensaje_r4;
+    }
+
+    public String getMensaje_r5() {
+        return mensaje_r5;
+    }
+
+    public void setMensaje_r5(String mensaje_r5) {
+        this.mensaje_r5 = mensaje_r5;
+    }
+
+    public float getPromedio() {
+        return promedio;
+    }
+
+    public void setPromedio(float promedio) {
+        this.promedio = promedio;
+    }
+
+   
+      
+      
+      
+}
+
---
+++ src/main/java/co/gov/mintic/misiontic/usa/ciclo3/modelo/RespuestaJson.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.gov.mintic.misiontic.usa.ciclo3.modelo;
+
+/**
+ *
+ * @author JOHN CARLOS ARRIETA ARRIETA
+ */
+public class RespuestaJson {
+
+    private Estudiante[] items;
+    private boolean hasMore;
+    private int limit;
+    private int offset;
+    private int count;
+    private Enlace[] links;
+
+    public Estudiante[] getItems() {
+        return items;
+    }
+
+    public void setItems(Estudiante[] items) {
+        this.items = items;
+    }
+
+    public boolean isHasMore() {
+        return hasMore;
+    }
+
+    public void setHasMore(boolean hasMore) {
+        this.hasMore = hasMore;
+    }
+
+    public int getLimit() {
+        return limit;
+    }
+
+    public void setLimit(int limit) {
+        this.limit = limit;
+    }
+
+    public int getOffset() {
+        return offset;
+    }
+
+    public void setOffset(int offset) {
+        this.offset = offset;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public Enlace[] getLinks() {
+        return links;
+    }
+
+    public void setLinks(Enlace[] links) {
+        this.links = links;
+    }
+
+}
+
+/*
+JsonRespuesta{
+  items: [
+    Estudiante{
+      nombre: Lina Paola Gonzalez Arias,
+      frontend_reto2;
+      frontend_reto3;
+      frontend_reto4;
+      frontend_reto5;
+      mensaje_r2;
+      mensaje_r3;
+      mensaje_r4;
+      mensaje_r5;
+      promedio: 5
+    }
+
+  ],
+  hasMore: false,
+  limit: 25,
+  offset: 0,
+  count: 1,
+  links: [
+    Enlaces{
+      rel: self,
+      href: https://g6c335b483ca254-bdgastosg16.adb.sa-santiago-1.oraclecloudapps.com/ords/admin/notas/retos/lina.gonzalez.mt@correo.usa.edu.co
+    },
+    {
+      rel: describedby,
+      href: https://g6c335b483ca254-bdgastosg16.adb.sa-santiago-1.oraclecloudapps.com/ords/admin/metadata-catalog/notas/retos/item
+    },
+    {
+      rel: first,
+      href: https://g6c335b483ca254-bdgastosg16.adb.sa-santiago-1.oraclecloudapps.com/ords/admin/notas/retos/lina.gonzalez.mt@correo.usa.edu.co
+    }
+  ]
+}
+ */
